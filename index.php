0d2df1344f0ab7a3e57958703ff5c1186225b42f
Dainelle Riley <dainellecriley@ufl.edu>
2016-02-18 06:31:16 -0500

    update index

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('css/style.css'); ?>" />
-<title>Dainelle Riley Blank Template</title>
-</head>
-<body>
-        <div class="container">
-            <header class="row">
-                <div class="twelve columns">
-                    <h1>Dainelle WordPress Template</h1>
-                    <p>This is Dainelle's WordPress template.</p>
-                </div>
-            </header>
-            <section class="row">
-                <div class="twelve columns">
-                    <p>Body content goes here.</p>
-                </div>
-            </section>
-            <footer class="row">
-                <div class="twelve columns">
-                    <p>Footer content goes here.</p>
-                </div>
-            </footer>
-        </div> <!-- ends container -->
-    </body>
-</html>
-
+<?php get_header(); ?>
+    <section class="row">
+        <div class="twelve columns">
+            <p>Body content goes here.</p>
+        </div>
+    </section>
+<?php get_footer(); ?>
