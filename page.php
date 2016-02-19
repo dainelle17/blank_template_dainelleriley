01f48e64379890e4646fa012183937f3b1ed98a2
Dainelle Riley <dainellecriley@ufl.edu>
2016-02-19 05:09:33 -0500

    add sidebar to page

--- page.php
+++ page.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
     <section class="row">
-        <div class="twelve columns">
+        <div class="nine columns">
     <!-- BEGIN PAGE PHP -->
             <?php if (have_posts()) : 
-                /* OUR DATA CONTEXT IS DEFINED  */      
                 while (have_posts()) : the_post(); ?> 
                     <h2><?php the_title(); ?></h2>
                     <?php the_content();
                 endwhile;
             endif; ?>
-<!-- END PAGE PHP -->
+        </div>
+         <div class="three columns">
+            <h3>Sidebar Text</h3>
+            <p>This is the sidebar content to put in here.</p>
         </div>
     </div>
 <?php get_footer(); ?>
