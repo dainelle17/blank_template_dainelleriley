b8e9b540f823c2e0ce18e6ca683f4afda4202e60
Dainelle Riley <dainellecriley@ufl.edu>
2016-02-19 05:26:15 -0500

    update single php

---
+++ functions.php
@@ -0,0 +1,52 @@
+<?php 
+/*-------------- Enable Widgets--------------- */
+
+function blank_widgets_init() {
+    register_sidebar( array(
+        'name' => ('First Widget'),
+        'id' => 'first-widget',
+        'description' => 'Widget for our sidebar on pages', 
+        'before_widget' => '<div class="widget-sidebar">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+    register_sidebar( array(
+        'name' => ('Footer Left'),
+        'id' => 'footer-left',
+        'description' => 'Widget for the left side of our footer', 
+        'before_widget' => '<div class="widget-sidebar widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+    register_sidebar( array(
+        'name' => ('Footer Middle'),
+        'id' => 'footer-middle',
+        'description' => 'Widget for the middle of our footer', 
+        'before_widget' => '<div class="widget-sidebar widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+    register_sidebar( array(
+        'name' => ('Footer Right'),
+        'id' => 'footer-right',
+        'description' => 'Widget for the right side of our footer', 
+        'before_widget' => '<div class="widget-sidebar widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'                        
+        ));
+}
+add_action('widgets_init', 'blank_widgets_init');
+
+/*-------------- Enable Menu --------------- */
+
+add_theme_support('menus');
+
+
+/*--- Enable Post Thumbnails ---*/
+add_theme_support( 'post-thumbnails' ); 
+
+?>
--- single.php
+++ single.php
@@ -1,15 +1,27 @@
 <?php get_header(); ?>
     <section class="row">
-        <div class="twelve columns">
-    <!-- BEGIN SINGLE PHP -->
-            <?php if (have_posts()) :
-                /* OUR DATA CONTEXT IS DEFINED  */              
-                while (have_posts()) : the_post(); ?> 
-                    <h2><?php the_title(); ?></h2>
-                    <?php the_content();
-                endwhile;
-            endif; ?>
-<!-- END SINGLE PHP -->
+        <div class="two columns">
+        <br/>
         </div>
-    </section>
+        <div class="eight columns">
+             <?php 
+             if ( have_posts() ) {
+                while ( have_posts() ) {
+                     the_post(); ?> 
+                    <h3><?php the_title(); ?></h3>
+                    <?php if ( has_post_thumbnail() ) {
+                        ?> <div class="single-post-thumbnail">
+                        <?php  the_post_thumbnail('large'); ?>
+                        </div>
+                        <?php
+                        }  
+                        the_content();
+                } // end while
+             }  // end if
+             ?>
+        </div>
+        <div class="two columns">
+                <br/>
+        </div>
+            </section>
 <?php get_footer(); ?>
