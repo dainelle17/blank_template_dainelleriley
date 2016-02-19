e220a954b99be84c333a2193843a5b3757e88ec9
Dainelle Riley <dainellecriley@ufl.edu>
2016-02-19 05:03:32 -0500

    add pages

---
+++ pages.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+    <section class="row">
+        <div class="twelve columns">
+            <h1>Our Page</h1>
+            <p>Filler content for our page.</p>
+        </div>
+    </section>
+<?php get_footer(); ?>
